e61667c8992319443911ae924972d98f0a54de39
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-04 20:27:38 +0300

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,19 @@
+import { readdir, mkdir, copyFile } from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+    const path = './src/fs';
+    const contentInFsFolder = await readdir(path);
+
+    if(!contentInFsFolder.includes('files') || contentInFsFolder.includes('files_copy')) {
+        throw new Error('FS operation failed');
+    }
+
+    await mkdir(`${path}/files_copy`);
+
+    const contentInFilesFolder = await readdir(`${path}/files`);
+    for (const file of contentInFilesFolder) {
+        await copyFile(`${path}/files/${file}`, `${path}/files_copy/${file}`);
+    }
 };
+
+copy().then();
