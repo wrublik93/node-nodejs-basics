900ad9541813e2289eeda2411a582e71e89a7c52
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-04 23:05:36 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import { readdir, readFile } from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+    const path = './src/fs/files';
+    const files = await readdir(path);
+
+    if(!files.includes('fileToRead.txt')) throw new Error('FS operation failed');
+
+    const data = await readFile(`${path}/fileToRead.txt`, 'utf8');
+
+    console.log(data);
 };
+
+read().then();
