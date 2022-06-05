fdc80c33c01fb3c64bdfe5b39f5e703f23677a04
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-05 16:35:19 +0300

    feat: implement stream:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,11 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 export const write = async () => {
-    // Write your code here 
+    const path = './src/streams/files';
+    const dest = createWriteStream(`${path}/fileToWrite.txt`, {encoding: 'utf-8'});
+
+    await pipeline(process.stdin, dest);
 };
+
+write().then();
