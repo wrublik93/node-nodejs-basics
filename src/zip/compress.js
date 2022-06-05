9f74cc72783c41af33e0b625de51035f2fc93dec
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-05 13:22:40 +0300

    fix: fix implemented zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,5 @@
 import { createGzip } from 'zlib';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 import { createReadStream, createWriteStream } from 'fs';
 
 export const compress = async () => {
@@ -8,14 +8,9 @@ export const compress = async () => {
     const source = createReadStream(`${path}/fileToCompress.txt`);
     const dest = createWriteStream(`${path}/archive.gz`);
 
-    pipeline(source, gzip, dest, (err) => {
-        if (err) {
-            console.error('Pipeline failed.', err);
-            process.exitCode = 1;
-        } else {
-            console.log('Pipeline succeeded. Check archive.gz.')
-        }
-    });
+    await pipeline(source, gzip, dest);
+
+    console.log('Pipeline succeeded. Check archive.gz.');
 };
 
 compress().then();
