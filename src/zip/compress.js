5be8bf8afee5b667a091d485695ff61410969fe9
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-05 12:48:39 +0300

    feat: implement zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,21 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+    const path = './src/zip/files';
+    const gzip = createGzip();
+    const source = createReadStream(`${path}/fileToCompress.txt`);
+    const dest = createWriteStream(`${path}/archive.gz`);
+
+    pipeline(source, gzip, dest, (err) => {
+        if (err) {
+            console.error('Pipeline failed.', err);
+            process.exitCode = 1;
+        } else {
+            console.log('Pipeline succeeded. Check archive.gz.')
+        }
+    });
 };
+
+compress().then();
