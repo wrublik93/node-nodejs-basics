b14be3bf542c22aafc4e944ebd5e033cd9042953
Ivan Vrublevsky <wrublik.93@mail.ru>
2022-06-05 13:35:46 +0300

    feat: implement zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+    const path = './src/zip/files';
+    const gzip = createUnzip();
+    const source = createReadStream(`${path}/archive.gz`);
+    const dest = createWriteStream(`${path}/fileToCompress.txt`);
+
+    await pipeline(source, gzip, dest);
+
+    console.log('Pipeline succeeded. Check fileToCompress.txt.');
 };
+
+decompress().then();
